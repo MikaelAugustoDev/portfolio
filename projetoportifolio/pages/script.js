44c9677432d50fd4c9ffffc7f8dc44d935610499
Mikael <midesenvolviment@gmail.com>
2023-01-18 11:58:02 -0300

    portfólio

---
+++ projetoportifolio/pages/script.js
@@ -0,0 +1,11 @@
+function ProjetoFormulario() {
+    window.location.href = "../form/index.html";
+}
+
+function ProjetoLuneStore() {
+    window.location.href = "../projetolunestore/index.html"
+}
+
+function ProjetoCalculadora() {
+    window.location.href = "../calculadora/index.html"
+}
